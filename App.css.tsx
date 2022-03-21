aa0a6337bbdad7b97168c88c84ebbafd4e453e57
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-21 13:39:31 +0200

    NEA-1 | Switch to class components

---
+++ App.css.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components/native";
+
+export const ContainerTextWrapper = styled.Text`
+    color: #E50914;
+`;
+
+export const SectionContainer = styled.View`
+    top: 25%;
+    padding: 8%;
+`;
+
+export const SectionTitle = styled.Text`
+    font-size: 24px;
+    font-weight: 600;
+    text-align: center;
+`;
+
+export const SectionDescription = styled.Text`
+    padding-top: 3%;
+    padding-bottom: 5%;
+    font-size: 18px;
+    font-weight: 400;
+    text-align: center;
+`;
+
+export const HighlightText = styled.Text`
+    font-weight: 700;
+    color: #E50914;
+`;
+
--- App.tsx
+++ App.tsx
@@ -1,58 +1,34 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {ContainerTextWrapper, HighlightText, SectionContainer, SectionDescription, SectionTitle} from "./App.css";
 
 const Section: React.FC<{
   companyName: string;
   title: string;
 }> = ({children, companyName, title}) => {
   return (
-    <View>
-      <Text style={[styles.sectionTitle]}>
-        <Text style={[styles.companyHighlight]}>{companyName}</Text> {title}
-      </Text>
-      <Text style={[styles.sectionDescription]}> {children}</Text>
-    </View>
+    <SectionContainer>
+      <SectionTitle>
+        <ContainerTextWrapper>{companyName}</ContainerTextWrapper> {title}
+      </SectionTitle>
+      <SectionDescription> {children}</SectionDescription>
+    </SectionContainer>
   );
 };
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.sectionContainer}>
+    <SectionContainer>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
           <Section companyName="NETFLIX" title="Entertainment">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            Edit <HighlightText>App.tsx</HighlightText> to change this
             screen and then come back to see your edits.
           </Section>
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </SectionContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    top: '43%',
-    padding: 8,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    textAlign: 'center',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    textAlign: 'center',
-  },
-  companyHighlight: {
-    fontWeight: '700',
-    color: '#E50914',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
