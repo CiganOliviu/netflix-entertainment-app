358e08c866f5d1ad280305fd87c4dae2b7cfdbd7
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-25 00:48:55 +0200

    NEA-2 | Setup basic components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { FooterStyle } from './Footer.style';
+
+const Footer = () => {
+    return (
+        <FooterStyle>
+            <Text>
+                Footer
+            </Text>
+        </FooterStyle>
+    )
+};
+
+export default Footer;
---
+++ src/components/StageBackground/StageBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { StageBackgroundStyle } from './StageBackground.style';
+
+const StageBackground = () => {
+    return (
+        <StageBackgroundStyle>
+            <Text>
+                StageBackground
+            </Text>
+        </StageBackgroundStyle>
+    )
+};
+
+export default StageBackground;
