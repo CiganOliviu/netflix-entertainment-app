41015057817be95aef916bfca10e4ca710ed8e2e
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-25 21:32:38 +0200

    NEA-2 | Update request url

--- src/backend/apiCalls.ts
+++ src/backend/apiCalls.ts
@@ -4,7 +4,7 @@ export const apiKey = '27b8ce8a261392c5a3122cb299ba28fe';
 export const backendUrl = 'https://api.themoviedb.org/3';
 
 export const requestUrls = {
-    fetchPopularMovies: `${backendUrl}/movie/popular?api_key=${apiKey}`,
+    fetchPopularMovies: `${backendUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=1`,
 };
 
 export const useCustomFetchPopularMovies = () => {
