14e7ba0627d64c4d3b9fcb83da4e62d816946b41
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-01 20:05:52 +0300

    NEA-19 | Setup splash screen for android devices

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, View } from 'react-native';
 import {
   ContainerTextWrapper,
@@ -6,6 +6,7 @@ import {
   SectionDescription,
   SectionTitle,
 } from './App.css';
+import SplashScreen from 'react-native-splash-screen';
 
 const Section: React.FC<{
   companyName: string;
@@ -22,6 +23,10 @@ const Section: React.FC<{
 };
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <SectionContainer>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
